0cc32925501bede90597e707a85d8ee43e3f3588
Lê Chí Luân <chiluan6601@gmail.com>
2022-11-02 07:51:47 +0700

    Change icon logo

--- app/src/main/java/com/example/m_expense/MainActivity.java
+++ app/src/main/java/com/example/m_expense/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.m_expense;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+//    private static int SPLASH_SCREEN = 5000;
+    //    Variable
+    Animation topAnim, bottomAnim;
+    ImageView imageLogo;
+    Button btnStart;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Animation
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+//       Hooks
+        imageLogo = findViewById(R.id.imageLogo);
+        btnStart = findViewById(R.id.btnStart);
+        imageLogo.setAnimation(topAnim);
+        btnStart.setAnimation(bottomAnim);
+        textView = findViewById(R.id.textView);
+        textView.setAnimation(bottomAnim);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TripActivity.class));
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/m_expense/TripActivity.java
@@ -0,0 +1,13 @@
+package com.example.m_expense;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip);
+    }
+}
